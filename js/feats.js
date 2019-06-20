abe4598b82d1fd5e906c2ae6f5702dbf2e88ed74
Unknown <q.necron@gmail.com>
2019-06-19 21:09:32 -0500

    More feats cleanup
    Mobile was a little off and the naming scheme was different from Spells.. corrected

--- js/feats.js
+++ js/feats.js
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function () {
         container.classList.add('feat', 'js-feat');
         name.classList.add('feat-name');
         type.classList.add('feat-type');
-        more.classList.add('feat-more', 'js-feat-expand');
+        more.classList.add('feat-expand', 'js-feat-expand');
         info.classList.add('feat-info', 'js-feat-info');
         description.classList.add('feat-description');
 
